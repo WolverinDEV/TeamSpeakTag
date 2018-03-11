cf6dbd7f0e582b44071d2b8fd8af021fde54d5d0
WolverinDEV <git@mcgalaxy.de>
2018-03-11 11:13:43 +0100

    Fixing realy anoing files

--- cmake-build-debug/test_decode.sh
+++
@@ -1,2 +0,0 @@
-JSON='[{"index":1,"type":2,"value":"Wj\ufffd\ufffd"},{"index":2,"type":2,"value":"MCGalaxy.de"},{"index":3,"type":2,"value":"KRJFJXo9m2WIqiMggKI5XYfhHNg="},{"index":5,"type":0,"value":9987},{"index":6,"type":0,"value":1520712396},{"index":8,"type":0,"value":1}]'
-echo ${JSON} | ./TS3DataParser encode cin
